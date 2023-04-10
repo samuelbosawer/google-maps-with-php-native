3de53e1de9b02ee99ff8f3cbb297e223f231c2b1
Samuel Bosawer <60697963+samuelbosawer@users.noreply.github.com>
2023-04-10 12:35:17 +0900

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-    $server = 'localhost';
-    $nama ="root";
-    $pass ="";
-    $db ="db_gis";
+    $server = 'gis.sacode.web.id';
+    $nama ="sakodeko_gis";
+    $pass ="sakodeko_gis";
+    $db ="sakodeko_gis";
 
     // Koneksi
     $koneksi = mysqli_connect($server, $nama, $pass, $db);
