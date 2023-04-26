5c0d06e17edfbc4437584dc2cb9a79665205aca7
Samuel Bosawer <zam.bosawer@gmail.com>
2023-04-26 11:40:27 +0900

    ✅ database

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,12 @@
 <?php
-    $server = 'feq50y.stackhero-network.com';
-    $nama ="root";
-    $pass ="mU2BSaSELn4jz7jBLRW4Hz0dgFVjKB0x";
-    $db ="db_gis";
+    $server = 'localhost';
+    $nama ="sakodeko_gis";
+    $pass ="sakodeko_gis";
+    $db ="sakodeko_gis";
+    // $server = 'feq50y.stackhero-network.com';
+    // $nama ="root";
+    // $pass ="mU2BSaSELn4jz7jBLRW4Hz0dgFVjKB0x";
+    // $db ="db_gis";
 
     // phpmyadmin https://feq50y.stackhero-network.com/
 
